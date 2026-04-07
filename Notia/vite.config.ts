399f697fb712f54c6a58778e7a41f1d46e5174f5
Huangxiong <3469287817@qq.com>
2026-04-07 19:35:14 +0800

    fixed vercel routing file for preview

--- Notia/vite.config.ts
+++ Notia/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/notia/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@supabase/supabase-js', 'tslib']
